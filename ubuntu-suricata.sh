6719fbe39f8bd27664b4a6f82be2c6fde4ae3203
mikejerez <mikejerez1@hotmail.com>
2023-01-01 12:20:14 -0500

    Create ubuntu-suricata.sh

---
+++ ubuntu-suricata.sh
@@ -0,0 +1,10 @@
+!# bin/bash
+
+# add the suricata repo
+sudo add-apt-repository ppa:oisf/suricata-stable
+
+# update repository
+sudo apt-get update
+
+# install suricata
+sudo apt-get install suricata -y
